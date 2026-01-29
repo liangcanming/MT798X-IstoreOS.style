01b011f723a39ae834c8b6e3be816a61f78c1ef9
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2026-01-29 19:29:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,10 +20,10 @@ sed -i '1i src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall
 sed -i '2i src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' feeds.conf.default
 sed -i '3i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
 sed -i '4i src-git OpenClash https://github.com/vernesong/OpenClash.git;master' feeds.conf.default
-sed -i '5i src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' feeds.conf.default
-sed -i '6i src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main' feeds.conf.default
-sed -i '7i src-git daed https://github.com/QiuSimons/luci-app-daed.git;master' feeds.conf.default
-sed -i '8i src-git ssr-plus https://github.com/fw876/helloworld.git;master' feeds.conf.default
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
+echo "src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main" >> "feeds.conf.default"
+echo "src-git daed https://github.com/QiuSimons/luci-app-daed;master" >> "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> "feeds.conf.default"
 
 # 插件添加
 git clone --depth=1 https://github.com/sirpdboy/luci-app-watchdog package/watchdog
